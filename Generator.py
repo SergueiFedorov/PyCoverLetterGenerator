e3f6b465072bfa874fb186f861f4e58e73e0a52b
blackraven36 <blackraven36@gmail.com>
2016-02-06 19:11:22 -0600

    Wrapped the code into a class

--- Generator.py
+++ Generator.py
@@ -2,72 +2,77 @@ from sys import argv
 import re
 import json
 
-def ReplaceFields(fieldName, value, text):
-	print fieldName
-	return re.sub("{" + fieldName + "}", value.encode('utf-8'), text)
+class Processor:
+	def __init__(self, qualifactionsFile, inputFile, outputFileName):
+		self.InputFileName = inputFile
+		self.QualificationsFile = qualifactionsFile
+		self.OutputFileName = outputFileName
 
-def LoadInputData():
-	filedata = open("input.json", "r").read()
-	return json.loads(filedata)
+	def Run(self):
+		inputData = self.__LoadInputData()
+		qualificationsData = self.__LoadQualificationsData()
+		outputLines = self.__BuildOutput(inputData, qualificationsData)
+		outputString = self.__BuildPreliminaryString(inputData, outputLines)
+		outputString = self.__ReplaceFieldsInString(inputData, outputString)
+		self.__WriteOutputFile(outputString)
 
-def LoadQualificationsData():
-	filedata = open("qualification.json", "r").read()
-	return json.loads(filedata)
+	def __LoadInputData(self):
+		filedata = open(self.InputFileName, "r").read()
+		return json.loads(filedata)
 
+	def __LoadQualificationsData(self):
+		filedata = open(self.QualificationsFile, "r").read()
+		return json.loads(filedata)
 
-def BuildOutput(inputData, qualificationsData):
-	if "items" not in inputData:
-		raise Exception("Please make sure the input file has an items element")
+	def __BuildOutput(self, inputData, qualificationsData):
+		if "items" not in inputData:
+			raise Exception("Please make sure the input file has an items element")
 
-	outputLines = []
-	for foundQualification in inputData["items"]:
-		found = False
-		for definedQualification in qualificationsData:
-			if (foundQualification.lower() == definedQualification["name"].lower()):
-				found = True
-				outputLines.append(definedQualification["text"])
+		outputLines = []
+		for foundQualification in inputData["items"]:
+			found = False
+			for definedQualification in qualificationsData:
+				if (foundQualification.lower() == definedQualification["name"].lower()):
+					found = True
+					outputLines.append(definedQualification["text"])
 
-		if found is False:
-			print "WARNING: Could not `find` %s qualifcation in qualification.json" % foundQualification
+			if found is False:
+				raise Exception("WARNING: Could not `find` %s qualifcation in qualification.json" % foundQualification)
 
-	return outputLines
+		return outputLines
 
-def BuildPreliminaryString(inputData, lines):
-	if "opening" not in inputData:
-		raise Exception("Please make sure the input file has an opening element")
+	def __BuildPreliminaryString(self, inputData, lines):
+		if "opening" not in inputData:
+			raise Exception("Please make sure the input file has an opening element")
 
-	if "itemsOpening" not in inputData:
-		raise Exception("Please make sure the input file has an itemsOpening element")	
+		if "itemsOpening" not in inputData:
+			raise Exception("Please make sure the input file has an itemsOpening element")	
 
-	if "closing" not in inputData:
-		raise Exception("Please make sure the input file has an closing element")	
+		if "closing" not in inputData:
+			raise Exception("Please make sure the input file has an closing element")	
 
-	items = [inputData["opening"].encode('utf-8'),
-			"\n\n",
-			inputData["itemsOpening"].encode('utf-8'), 
-			" ",
-			" ".join(lines).encode('utf-8'),
-			"\n\n",
-			 inputData["closing"].encode('utf-8'), 
-			"\n\n"]
-	return "".join(items)
+		items = [inputData["opening"],
+				"\n\n",
+				inputData["itemsOpening"], 
+				" ",
+				" ".join(lines),
+				"\n\n",
+				 inputData["closing"], 
+				"\n\n"]
+		return "".join(items)
 
-def ReplaceFieldsInString(inputData, string):
-	for replaceField, replaceValue in inputData["fields"].iteritems():
-		string = ReplaceFields(replaceField, replaceValue, string)
+	def __ReplaceFieldsInString(self, inputData, string):
+		for replaceField, replaceValue in inputData["fields"].items():
+			string = self.__ReplaceFields(replaceField, replaceValue, string)
+		return string
 
-	return string
+	def __ReplaceFields(self, fieldName, value, text):
+		print (fieldName)
+		return re.sub("{" + fieldName + "}", value, text)	
 
-def WriteOutputFile(string):
-	outputFile = open("output.txt", "w")
-	outputFile.write(string)
+	def __WriteOutputFile(self, string):
+		outputFile = open(self.OutputFileName, "w")
+		outputFile.write(string)	
 
-def Run():
-	inputData = LoadInputData()
-	qualificationsData = LoadQualificationsData()
-	outputLines = BuildOutput(inputData, qualificationsData)
-	outputString = BuildPreliminaryString(inputData, outputLines)
-	outputString = ReplaceFieldsInString(inputData, outputString)
-	WriteOutputFile(outputString)
-
-Run()
+processor = Processor("qualification.json", "input.json", "output.txt")
+processor.Run()
